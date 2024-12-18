392f8550c0f03ce4598e63b9d7fe93c17a636069
yosef_adrian <yosef.adrian@unpar.ac.id>
2024-12-18 15:04:35 +0700

    update

--- src/components/CharacterSelection.jsx
+++ src/components/CharacterSelection.jsx
@@ -12,7 +12,7 @@ const CharacterSelection = () => {
   // List of character models
   const models = [
     { name: "Tororo", url: "https://raw.githubusercontent.com/zenghongtu/live2d-model-assets/master/assets/moc/tororo/tororo.model.json" },
-    { name: "Wanko", url: "/cubism/wanko/wanko/runtime/wanko_touch.model3.json" },
+    { name: "Wanko", url: "http://localhost:5174//cubism/wanko/wanko/runtime/wanko_touch.model3.json" },
     { name: "Senko", url: "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json" },
     { name: "Shizuku", url: "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/shizuku/shizuku.model.json" },
     { name: "Shizuku", url: "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/shizuku/shizuku.model.json" },
@@ -28,7 +28,17 @@ const CharacterSelection = () => {
     canvasRef.current.appendChild(pixiApp.view);
     setApp(pixiApp);
 
+    // Add resize re render 
+    const handleResize = () => {
+      const model = pixiApp.stage.children[0];
+      if (model) {
+        model.position.set(window.innerWidth / 2, window.innerHeight / 2);
+        model.scale.set(0.2, 0.2);
+      }
+    };
+    window.addEventListener("resize", handleResize);
     return () => {
+      window.removeEventListener("resize", handleResize);
       pixiApp.destroy(true, { children: true, texture: true, baseTexture: true });
     };
   }, []);
@@ -37,7 +47,13 @@ const CharacterSelection = () => {
     if (!app || !selectedModel) return;
 
     // Hapus semua anak sebelum menambahkan yang baru
-    app.stage.removeChildren();
+    // app.stage.removeChildren();
+
+    // Check if app.stage is defined before removing children
+    if (app.stage) {
+      app.stage.removeChildren(); // Remove all children before adding the new model
+    }
+
     const canvasWidth = canvasRef.current.offsetWidth;
     const canvasHeight = canvasRef.current.offsetHeight;
 
